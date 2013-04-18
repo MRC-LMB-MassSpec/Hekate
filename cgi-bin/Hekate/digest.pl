2e236b00aaf6034f3d2fc244b6db432706efe7e5
Andrew Holding <andy-git@evath.net>
2013-04-18 16:16:17 +0100

    Fixed bug in loading modifications by Hekate Digest

--- cgi-bin/Hekate/digest.pl
+++ cgi-bin/Hekate/digest.pl
@@ -62,7 +62,7 @@ my $results_table = save_settings(
 );
 
 
-my %protein_residuemass = protein_residuemass($results_table);
+my %protein_residuemass = protein_residuemass($results_table, $dbh);
 my %modifications = modifications($mono_mass_diff, $xlinker_mass, $reactive_site, $results_table, $dbh);
 
 #Output page
